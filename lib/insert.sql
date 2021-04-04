b4e99e19e5af799909f5134f821a68cd0c4ee1d6
Janis Lebron <janislebron@gmail.com>
2021-04-04 11:07:33 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Mr. Chocolate", 7, "male", "calm", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Rowdy", 3, "female", "calm", 0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Tabitha", 1, "male", "calm", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Seargeant Brown", 4, "female", "calm", 0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Melissa", 6, "male", "calm", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Grinch", 5, "female", "calm", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Wendy", 8, "male", "calm", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES(NULL, 2, "male", "calm", 0);
