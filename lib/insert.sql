7a4f03cb9fee3765b80374191942344aa80f1028
Janis Lebron <janislebron@gmail.com>
2021-04-04 11:10:37 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Mr. Chocolate", 7, "male", "calm", 1);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Rowdy", 3, "female", "calm", 0);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Tabitha", 1, "male", "calm", 1);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Seargeant Brown", 4, "female", "calm", 0);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Melissa", 6, "male", "calm", 1);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Grinch", 5, "female", "calm", 1);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Wendy", 8, "male", "calm", 1);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES(NULL, 2, "male", "calm", 0);
+insert into bears (id,name,age,gender,color,temperament,alive) values (1,'Mr. Chocolate',10,'M','black','hot',0);
+insert into bears (id,name,age,gender,color,temperament,alive) values (2,'Rowdy',20,'M','blue','hot',1);
+insert into bears (id,name,age,gender,color,temperament,alive) values (3,'Tabitha',30,'F','red','hot',0);
+insert into bears (id,name,age,gender,color,temperament,alive) values (4,'Sergeant Brown',40,'M','yellow','hot',0);
+insert into bears (id,name,age,gender,color,temperament,alive) values (5,'Melissa',50,'M','black','pink',1);
+insert into bears (id,name,age,gender,color,temperament,alive) values (6,'Grinch',60,'F','black','white',0);
+insert into bears (id,name,age,gender,color,temperament,alive) values (7,'Wendy',70,'M','black','grey',0);
+insert into bears (id,name,age,gender,color,temperament,alive) values (8,NULL,80,'M','black','magenta',1);
